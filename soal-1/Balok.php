53b83c48c887078b9937b4ce289a5f758c43b365
Yoga_Axel_Baim_Pratama <23_yogaxelbaim@student.smkti.net>
2024-10-03 21:26:41 +0800

    Selesai

--- soal-1/Balok.php
+++ soal-1/Balok.php
@@ -2,23 +2,69 @@
 class Balok
 {
     // deklarasikan atribut-atribut kelas Balok
-
+    private $panjang;
+    private $lebar;
+    private $tinggi;
     
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($panjang, $lebar, $tinggi)
+    {
+        $this->panjang = $panjang;
+        $this->lebar = $lebar;
+        $this->tinggi = $tinggi;
+    }
+
+    public function setPanjang($panjang)
+    {
+        $this->panjang = $panjang;
+    }
+
+    public function setLebar($lebar)
+    {
+        $this->lebar = $lebar;
+    }
+
+    public function setTinggi($tinggi)
+    {
+        $this->tinggi = $tinggi;
+    }
+
+    // Metode asesor (getter) untuk mendapatkan nilai panjang
+    public function getPanjang()
+    {
+        return $this->panjang;
+    }
+
+    public function getLebar()
+    {
+        return $this->lebar;
+    }
+
+    public function getTinggi()
+    {
+        return $this->tinggi;
+    }
 
 
     // definisikan metode menghitung volume, luas permukaan, 
+    // Metode untuk menghitung volume
+    public function hitungVolume()
+    {
+        return $this->panjang * $this->lebar  * $this->tinggi;
+    }
+
+    public function hitungLuasPermukaan()
+    {
+        return 2 * ($this->panjang * $this->lebar + $this->panjang * $this->tinggi + $this->lebar * $this->tinggi);
+    }
 }
+
 
 $balok_1 = new Balok(4, 8, 5);
 $balok_2 = new Balok(3, 10, 12);
 
-// contoh output: Balok
+echo "Volume Balok: " . $balok_1->hitungVolume() . "<br>";
+echo "Luas Permukaan Balok: " . $balok_1->hitungLuasPermukaan() . "<br>";
 
-// Balok 1
-// Volume: 160
-// Luas Permukaan: 184
-
-// Balok 2
-// Volume: 360
-// Luas Permukaan: 372
+echo "Volume Balok: " . $balok_2->hitungVolume() . "<br>";
+echo "Luas Permukaan Balok: " . $balok_2->hitungLuasPermukaan() . "<br>";
